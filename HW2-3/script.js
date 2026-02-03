463bb86fc756d96f07228fe2e88ae9a350a83e93
Anabel Assante <anabel.assante@gmail.com>
2026-02-02 19:08:01 -0700

    Commit HW3

---
+++ HW2-3/script.js
@@ -0,0 +1,26 @@
+/*
+Author: Anabel Assante
+File Name: HW3 index.html
+Date: 02/02/2026
+Purpose: Introduction About Me
+*/
+
+//My favorite Video Games log output.  Compiled due to short answer.
+console.log("Three of my current favorite games are: RDR2, Peak, and Fields of Mistria!");
+
+//My favorite websites log output.  Compiled due to straight-forward answers.
+console.log("Three of my favorite websites are: Youtube (for art tips/advice), Stack Overflow (help with coding issues), and Pinterest (art references).");
+
+//My three favorite artists output.  Separated for long reasoning.
+console.log("My first favorite artist is Miya (@ni053791 on Instagram) their emotes are wonderful, which I find myself using in my personal life.  Their linework and use of colors has always inspired my own art and rendering, the impact they have is on my own culture using their emojis and art to engage with others.");
+console.log("My second favorite artist, a band named Poor Man's Poison.  Their work and music have inspired many characters and artwork of mine, everywhere from NPC's to main villains/heroes.");
+console.log("My third favorite artist is Jorge Rivera-Herrans, his work with the EPIC musical has brought a lot of artists and history nerds alike together.  Seeing the way his music have inspired new waves of artists to create animations or fanwork, and how he often works with these artists has impacted fandom and online art culture a lot in recent years.");
+
+//Troubleshooting Recommendation - image struggles to load before the prompt in some cases.  Required for prompt.
+console.log("Side Note: If Bella the Coding Cat is not showing up on your html, please cancel this prompt and reload the page.  She demands your input!");
+
+//Q & A with user prompt.
+var adjective = prompt("What word comes to mind to describe Bella the Coding Cat?");
+
+console.log("Bella the Coding Cat is very pleased that you think she is " + adjective);
+
